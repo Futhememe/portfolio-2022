5c1ff478aa54e01bcae207128f6e4e195f9dc42b
Futhememe <kyoro_souza@hotmail.com>
2022-04-04 22:20:14 -0300

    💄 add menu icon

---
+++ src/components/Icons.tsx
@@ -0,0 +1,10 @@
+import { createIcon } from '@chakra-ui/icons'
+
+export const MenuIcon = createIcon({
+  displayName: 'MenuIcon',
+  viewBox: '0 0 24 24',
+
+  path: (
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M3 16H21V18H3V16ZM3 11H15V13H3V11ZM3 6H21V8H3V6Z" fill="black"/>
+  ),
+})
